9e607522620b2f8c30f14118f02bd97e8e7d5022
Jēkabs Milbrets <jekabs.milbrets@me.com>
2022-02-20 15:16:50 +0200

    Updated View TasksComponent;

--- src/app/views/tasks/tasks.component.ts
+++ src/app/views/tasks/tasks.component.ts
@@ -1,15 +1,59 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, take } from 'rxjs';
+import { Task } from 'src/app/models/task.model';
+import { TasksService } from 'src/app/services/tasks.service';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.scss']
+  styleUrls: ['./tasks.component.scss'],
 })
 export class TasksComponent implements OnInit {
+  public tasks$: Observable<Task[]>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private tasksService: TasksService,
+  ) {
+    this.tasks$ = this.tasksService.tasks$;
   }
 
+  public ngOnInit(): void {
+    this.tasksService.list()
+        .pipe(
+          take(1),
+        )
+        .subscribe();
+  }
+
+  public createTask(): void {
+    const task = new Task({
+      name: 'MTET-' + (Math.floor(Math.random() * 10000) + 10000).toString().substring(1),
+    });
+
+    this.tasksService.create(task)
+        .pipe(
+          take(1),
+        )
+        .subscribe();
+  }
+
+  public onEdit(task: Task): void {
+    this.tasksService.update(task)
+        .pipe(
+          take(1),
+        )
+        .subscribe();
+  }
+
+  public onRemove(task: Task): void {
+    this.tasksService.delete(task)
+        .pipe(
+          take(1),
+        )
+        .subscribe();
+  }
+
+  public onStartTime(task: Task): void {
+    console.log('onStartTime ', {task});
+  }
 }
