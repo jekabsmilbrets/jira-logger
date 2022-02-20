83f56650c8023f4b1062cf5756abe7996798ee7d
Jēkabs Milbrets <jekabs.milbrets@me.com>
2022-02-20 20:05:29 +0200

    Updated TaskComponent - implemented use of TaskUpdateActionEnum;

--- src/app/modules/task/components/task/task.component.ts
+++ src/app/modules/task/components/task/task.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, EventEmitter, Output } from '@angular/core';
 
+import { TaskUpdateActionEnum } from '@task/enums/task-update-action.enum';
+
 import { Task } from '@task/models/task.model';
 
 @Component({
@@ -10,11 +12,14 @@ import { Task } from '@task/models/task.model';
 export class TaskComponent {
   @Input() task!: Task;
 
-  @Output() update: EventEmitter<Task> = new EventEmitter<Task>();
+  @Output() update: EventEmitter<[Task, TaskUpdateActionEnum]> = new EventEmitter<[Task, TaskUpdateActionEnum]>();
   @Output() remove: EventEmitter<Task> = new EventEmitter<Task>();
 
   public onUpdate(task: Task): void {
-    this.update.emit(task);
+    this.update.emit([
+      task,
+      TaskUpdateActionEnum.update,
+    ]);
   }
 
   public onRemove(task: Task): void {
@@ -22,12 +27,18 @@ export class TaskComponent {
   }
 
   public onToggleTimeLog(task: Task): void {
+    let action: TaskUpdateActionEnum;
     if (!task.lastTimeLogId) {
       task.startTimeLog();
+      action = TaskUpdateActionEnum.startWorkLog;
     } else {
       task.stopTimeLog();
+      action = TaskUpdateActionEnum.stopWorkLog;
     }
 
-    this.update.emit(task);
+    this.update.emit([
+      task,
+      action,
+    ]);
   }
 }
