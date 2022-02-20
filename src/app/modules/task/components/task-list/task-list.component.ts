bc8a1d9dba4fdd84644890cfdee73f186b285200
Jēkabs Milbrets <jekabs.milbrets@me.com>
2022-02-20 19:13:38 +0200

    Updated TaskListComponent: * Renamed edit => update emitter; * Removed startTime emitter;

--- src/app/modules/task/components/task-list/task-list.component.ts
+++ src/app/modules/task/components/task-list/task-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
-import { Task } from '../../models/task.model';
+import { Task } from '@task/models/task.model';
 
 @Component({
   selector: 'app-task-list',
@@ -12,23 +12,16 @@ export class TaskListComponent {
   public tasks: Task[] | null = [];
 
   @Output()
-  public edit: EventEmitter<Task> = new EventEmitter<Task>();
+  public update: EventEmitter<Task> = new EventEmitter<Task>();
 
   @Output()
   public remove: EventEmitter<Task> = new EventEmitter<Task>();
 
-  @Output()
-  public startTime: EventEmitter<Task> = new EventEmitter<Task>();
-
-  public onEdit(task: Task): void {
-    this.edit.emit(task);
+  public onUpdate(task: Task): void {
+    this.update.emit(task);
   }
 
   public onRemove(task: Task): void {
     this.remove.emit(task);
-  }
-
-  public onStartTime(task: Task): void {
-    this.startTime.emit(task);
   }
 }
