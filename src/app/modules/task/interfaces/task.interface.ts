2b6ecb9ee5008b98e8eb4b5ec692d66d3af15eaa
Jēkabs Milbrets <jekabs.milbrets@me.com>
2022-02-20 16:52:27 +0200

    Created TimeLog, TimeLogs in Task Interface and Model;

--- src/app/modules/task/interfaces/task.interface.ts
+++ src/app/modules/task/interfaces/task.interface.ts
@@ -1,5 +1,11 @@
+import { TimeLogInterface } from './time-log.interface';
+
 export interface TaskInterface {
   createDate: Date;
 
   name: string;
+
+  lastTimeLogId?: number;
+
+  timeLogs: { [key: number]: TimeLogInterface };
 }
--- src/app/modules/task/models/task.model.ts
+++ src/app/modules/task/models/task.model.ts
@@ -1,9 +1,15 @@
 import { TaskInterface } from '../interfaces/task.interface';
+import { TimeLogs } from '../types/time-logs.type';
+
+import { TimeLog } from './time-log.model';
 
 export class Task implements TaskInterface {
+  private _name!: string;
+
   private _createDate!: Date;
 
-  private _name!: string;
+  private _lastTimeLogId!: number;
+  private _timeLogs: TimeLogs = {};
 
   constructor(data?: Partial<TaskInterface>) {
     Object.assign(this, data);
@@ -21,6 +27,7 @@ export class Task implements TaskInterface {
     this._name = value;
   }
 
+
   public get createDate(): Date {
     if (!this._createDate) {
       this.createDate = new Date();
@@ -31,5 +38,51 @@ export class Task implements TaskInterface {
 
   public set createDate(value: Date) {
     this._createDate = value;
+  }
+
+
+  public get lastTimeLogId(): number {
+    return this._lastTimeLogId;
+  }
+
+  public set lastTimeLogId(value: number) {
+    this._lastTimeLogId = value;
+  }
+
+  public get timeLogs(): TimeLogs {
+    return this._timeLogs;
+  }
+
+  public set timeLogs(value: TimeLogs) {
+    this._timeLogs = value;
+  }
+
+  public startTimeLog(description?: string): number {
+    const startTime = new Date();
+    const timeLog = new TimeLog({
+      id: startTime.getTime(),
+      startTime,
+      description,
+    });
+
+    this.timeLogs[timeLog.id] = timeLog;
+
+    this.lastTimeLogId = timeLog.id;
+
+    return timeLog.id;
+  }
+
+  public stopTimeLog(id?: number): void {
+    if (!id) {
+      id = this.lastTimeLogId;
+    }
+
+    if (!this.timeLogs.hasOwnProperty(id)) {
+      throw new Error(`Could not find time log for ID: "${id}" for task "${this.name}"`);
+    }
+
+    const timeLog: TimeLog = this.timeLogs[id];
+
+    timeLog.endTime = new Date();
   }
 }
