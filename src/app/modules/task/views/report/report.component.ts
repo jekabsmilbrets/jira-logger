6b5ec7c80f9c3672706e4d56b331cf7f9f6fed8f
Jēkabs Milbrets <jekabs.milbrets@me.com>
2022-02-21 19:09:29 +0200

    JL-4: Updated Reports view with total worked times per task in table view with sorting and pagination;

--- src/app/modules/task/views/report/report.component.ts
+++ src/app/modules/task/views/report/report.component.ts
@@ -1,9 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+
+import { take, tap, Subscription } from 'rxjs';
+
+import { Task } from '@task/models/task.model';
+
+import { TasksService } from '@task/services/tasks.service';
+
 
 @Component({
   selector: 'app-report',
   templateUrl: './report.component.html',
   styleUrls: ['./report.component.scss'],
 })
-export class ReportComponent {
+export class ReportComponent implements OnInit, AfterViewInit, OnDestroy {
+
+  public dataSource: MatTableDataSource<Task> = new MatTableDataSource<Task>([]);
+
+  @ViewChild(MatSort)
+  private sort!: MatSort;
+  @ViewChild(MatPaginator)
+  private paginator!: MatPaginator;
+
+  private tasksSubscription!: Subscription;
+
+  constructor(
+    private tasksService: TasksService,
+  ) {
+    this.tasksService.tasks$
+        .pipe(
+          tap((data: Task[]) => this.dataSource.data = data),
+        )
+        .subscribe();
+  }
+
+  public ngOnInit(): void {
+    this.tasksService.list()
+        .pipe(
+          take(1),
+        )
+        .subscribe();
+  }
+
+  public ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+
+  public ngOnDestroy(): void {
+    this.tasksSubscription.unsubscribe();
+  }
 }
