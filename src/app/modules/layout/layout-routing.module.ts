f19cfd42c1656539fe0397ff21ad848922bab79b
Jēkabs Milbrets <jekabs.milbrets@me.com>
2022-02-21 20:06:34 +0200

    JL-16: Created LayoutModule: * HeaderComponent * SideNavComponent * SideNavInterface * LayoutComponent View * PageNotFoundComponent View

---
+++ src/app/modules/layout/layout-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { LayoutComponent } from './views/layout/layout.component';
+import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'tasks',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'tasks',
+        loadChildren: () => import('@task/task.module').then(m => m.TaskModule),
+      },
+      {
+        path: '**',
+        component: PageNotFoundComponent,
+      },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class LayoutRoutingModule {
+}
