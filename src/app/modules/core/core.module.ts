ffc84bfbc6aba523b38d581bcaae1f207f9a0632
Jēkabs Milbrets <jekabs.milbrets@me.com>
2022-02-23 23:35:17 +0200

    JL-9: Created DynamicMenu Directive, Interface, Model, Service

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 
+import { DynamicMenuDirective } from './directives/dynamic-menu.directive';
+import { DynamicMenuService } from './services/dynamic-menu.service';
 import { StorageService } from './services/storage.service';
 
 @NgModule({
   providers: [
     StorageService,
+    DynamicMenuService,
+  ],
+  declarations: [
+    DynamicMenuDirective,
+  ],
+  exports: [
+    DynamicMenuDirective,
   ],
 })
 export class CoreModule {
---
+++ src/app/modules/core/models/dynamic-menu.spec.ts
@@ -0,0 +1,7 @@
+import { DynamicMenu } from './dynamic-menu';
+
+describe('DynamicMenu', () => {
+  it('should create an instance', () => {
+    expect(new DynamicMenu()).toBeTruthy();
+  });
+});
