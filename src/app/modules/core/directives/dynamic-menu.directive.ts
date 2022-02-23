d70238846eac990c20113468aefe4c17e0fb36dd
Jēkabs Milbrets <jekabs.milbrets@me.com>
2022-02-23 23:35:17 +0200

    JL-9: Created DynamicMenu Directive, Interface, Model, Service

---
+++ src/app/modules/core/directives/dynamic-menu.directive.ts
@@ -0,0 +1,9 @@
+import { Directive, ViewContainerRef } from '@angular/core';
+
+@Directive({
+  selector: '[appDynamicMenu]',
+})
+export class DynamicMenuDirective {
+  constructor(public viewContainerRef: ViewContainerRef) {
+  }
+}
