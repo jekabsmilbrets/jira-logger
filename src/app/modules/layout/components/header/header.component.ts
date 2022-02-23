6c314b55f7f7c4584543a9a7dbe3407b04076a4c
Jēkabs Milbrets <jekabs.milbrets@me.com>
2022-02-23 23:36:26 +0200

    JL-9: Implemented DynamicMenu injection into LayoutModel HeaderComponent;

--- src/app/modules/layout/components/header/header.component.ts
+++ src/app/modules/layout/components/header/header.component.ts
@@ -1,12 +1,72 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild, OnDestroy } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
+
+import { Router, NavigationEnd } from '@angular/router';
+
+import { Subscription, filter, switchMap, take, tap, Observable } from 'rxjs';
+
+import { DynamicMenuDirective } from '@core/directives/dynamic-menu.directive';
+import { DynamicMenuInterface } from '@core/interfaces/dynamic-menu.interface';
+import { DynamicMenu } from '@core/models/dynamic-menu';
+import { DynamicMenuService } from '@core/services/dynamic-menu.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnDestroy {
   @Input()
   public sidenav!: MatSidenav;
+
+  @ViewChild(DynamicMenuDirective, {static: true})
+  private dynamicMenu!: DynamicMenuDirective;
+
+  private routerEventsSubscription: Subscription;
+
+
+  constructor(
+    private dynamicMenuService: DynamicMenuService,
+    private router: Router,
+  ) {
+    this.routerEventsSubscription = this.router.events
+                                        .pipe(
+                                          filter(
+                                            (e): e is NavigationEnd => e instanceof NavigationEnd,
+                                          ),
+                                          switchMap(
+                                            (navigationEndEvent: NavigationEnd) => this.loadDynamicMenu(navigationEndEvent),
+                                          ),
+                                        )
+                                        .subscribe();
+  }
+
+  public ngOnDestroy(): void {
+    this.routerEventsSubscription.unsubscribe();
+  }
+
+  private loadDynamicMenu(navigationEndEvent: NavigationEnd): Observable<DynamicMenu[]> {
+    return this.dynamicMenuService.dynamicMenus$
+               .pipe(
+                 take(1),
+                 tap(
+                   (dynamicMenus: DynamicMenu[]) => {
+                     console.log({...navigationEndEvent});
+                     const dynamicMenu: DynamicMenu | undefined = dynamicMenus.find(
+                       (dM: DynamicMenu) => dM.data.route === navigationEndEvent.url,
+                     );
+
+                     if (dynamicMenu) {
+                       const viewContainerRef = this.dynamicMenu.viewContainerRef;
+                       viewContainerRef.clear();
+
+                       viewContainerRef.createComponent<DynamicMenuInterface>(dynamicMenu.component);
+                     } else {
+                       const viewContainerRef = this.dynamicMenu.viewContainerRef;
+                       viewContainerRef.clear();
+                     }
+                   },
+                 ),
+               );
+  }
 }
