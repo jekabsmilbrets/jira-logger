7575eefd3776560462ee7a995b7c6d3d29c2c894
Jēkabs Milbrets <jekabs.milbrets@me.com>
2022-02-24 20:13:57 +0200

    JL-9: Implemented SharedModule in TaskModule;

--- src/app/modules/task/task.module.ts
+++ src/app/modules/task/task.module.ts
@@ -1,16 +1,7 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
-import { MatTableModule } from '@angular/material/table';
-import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { SharedModule } from '@shared/shared.module';
 
 import { ReportModeSwitcherComponent } from './components/report-mode-switcher/report-mode-switcher.component';
 import { TaskListComponent } from './components/task-list/task-list.component';
@@ -32,19 +23,9 @@ import { TasksComponent } from './views/tasks/tasks.component';
     ReportModeSwitcherComponent,
   ],
   imports: [
-    CommonModule,
     ReactiveFormsModule,
     TaskRoutingModule,
-    MatCardModule,
-    MatIconModule,
-    MatButtonModule,
-    MatButtonToggleModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatTableModule,
-    MatSortModule,
-    MatPaginatorModule,
-    MatToolbarModule,
+    SharedModule,
   ],
   providers: [
     TasksService,
