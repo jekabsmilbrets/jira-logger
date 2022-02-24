d0519aa8cb681472c27c4624c71ff09317804990
Jēkabs Milbrets <jekabs.milbrets@me.com>
2022-02-24 20:13:27 +0200

    JL-9: Implemented SharedModule in LayoutModule;

--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -1,19 +1,14 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { CoreModule } from '@core/core.module';
+
+import { SharedModule } from '@shared/shared.module';
 
 import { HeaderComponent } from './components/header/header.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { LayoutRoutingModule } from './layout-routing.module';
 import { LayoutComponent } from './views/layout/layout.component';
 import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
-
 
 @NgModule({
   declarations: [
@@ -23,14 +18,9 @@ import { PageNotFoundComponent } from './views/page-not-found/page-not-found.com
     SidenavComponent,
   ],
   imports: [
-    CommonModule,
     LayoutRoutingModule,
-    MatSidenavModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-    MatListModule,
     CoreModule,
+    SharedModule,
   ],
 })
 export class LayoutModule {
