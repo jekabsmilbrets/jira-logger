d394cb70c010dac1d2aafbad2989185e1a0a75e3
Jēkabs Milbrets <jekabs.milbrets@me.com>
2022-02-24 20:44:01 +0200

    JL-9: Updated ReportComponent, added dynamic columns selected via reportMode @ ReportService;

--- src/app/modules/task/views/report/report.component.ts
+++ src/app/modules/task/views/report/report.component.ts
@@ -8,16 +8,12 @@ import { SharedModule } from 'app/modules/shared/shared.module';
 import { take, tap, Subscription } from 'rxjs';
 
 import { DynamicMenu } from '@core/models/dynamic-menu';
-
 import { DynamicMenuService } from '@core/services/dynamic-menu.service';
 
 import { ReportModeSwitcherComponent } from '@task/components/report-mode-switcher/report-mode-switcher.component';
 import { ReportModeEnum } from '@task/enums/report-mode.enum';
-
-
 import { Task } from '@task/models/task.model';
 import { ReportService } from '@task/services/report.service';
-
 import { TasksService } from '@task/services/tasks.service';
 
 @Component({
@@ -28,12 +24,22 @@ import { TasksService } from '@task/services/tasks.service';
 export class ReportComponent implements OnInit, AfterViewInit, OnDestroy {
   public dataSource: MatTableDataSource<Task> = new MatTableDataSource<Task>([]);
 
+  public columns: string[] = [];
+
   @ViewChild(MatSort)
   private sort!: MatSort;
   @ViewChild(MatPaginator)
   private paginator!: MatPaginator;
 
   private tasksSubscription!: Subscription;
+
+  private modeColumns: { [key: string]: string[] } = {
+    total: [
+      'name',
+      'description',
+      'timeLoggedToday',
+    ],
+  };
 
   constructor(
     private tasksService: TasksService,
@@ -61,6 +67,11 @@ export class ReportComponent implements OnInit, AfterViewInit, OnDestroy {
                                    tap((data: Task[]) => this.dataSource.data = data),
                                  )
                                  .subscribe();
+
+    this.reportService.reportMode$
+        .subscribe(
+          (reportMode: ReportModeEnum) => this.columns = this.modeColumns[reportMode],
+        );
   }
 
   public ngOnInit(): void {
