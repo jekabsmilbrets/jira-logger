866ecae38b8803a68efe40f290732b80cdc8da9f
Jēkabs Milbrets <jekabs.milbrets@me.com>
2022-02-24 20:23:46 +0200

    JL-9: Updated ReportComponent

--- src/app/modules/task/views/report/report.component.ts
+++ src/app/modules/task/views/report/report.component.ts
@@ -3,6 +3,8 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 
+import { SharedModule } from 'app/modules/shared/shared.module';
+
 import { take, tap, Subscription } from 'rxjs';
 
 import { DynamicMenu } from '@core/models/dynamic-menu';
@@ -10,9 +12,11 @@ import { DynamicMenu } from '@core/models/dynamic-menu';
 import { DynamicMenuService } from '@core/services/dynamic-menu.service';
 
 import { ReportModeSwitcherComponent } from '@task/components/report-mode-switcher/report-mode-switcher.component';
+import { ReportModeEnum } from '@task/enums/report-mode.enum';
 
 
 import { Task } from '@task/models/task.model';
+import { ReportService } from '@task/services/report.service';
 
 import { TasksService } from '@task/services/tasks.service';
 
@@ -34,15 +38,24 @@ export class ReportComponent implements OnInit, AfterViewInit, OnDestroy {
   constructor(
     private tasksService: TasksService,
     private dynamicMenuService: DynamicMenuService,
+    private reportService: ReportService,
   ) {
     this.dynamicMenuService.addDynamicMenu(
       new DynamicMenu(
         ReportModeSwitcherComponent,
         {
           route: '/tasks/report',
+          providers: [
+            {
+              provide: ReportService,
+              useValue: this.reportService,
+            },
+            SharedModule,
+          ],
         },
       ),
     );
+
     this.tasksSubscription = this.tasksService.tasks$
                                  .pipe(
                                    tap((data: Task[]) => this.dataSource.data = data),
