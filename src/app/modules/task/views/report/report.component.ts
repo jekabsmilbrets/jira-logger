629123792c770380285423c68be531a48471e988
Jēkabs Milbrets <jekabs.milbrets@me.com>
2022-02-27 21:35:20 +0200

    JL-10: Implemented Shared TableComponent in view ReportComponent;

--- src/app/modules/task/views/report/report.component.ts
+++ src/app/modules/task/views/report/report.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
+import { Component, OnInit } from '@angular/core';
 
 import { SharedModule } from 'app/modules/shared/shared.module';
 
-import { take, tap, Subscription } from 'rxjs';
+import { take, Observable } from 'rxjs';
 
 import { DynamicMenu } from '@core/models/dynamic-menu';
 import { DynamicMenuService } from '@core/services/dynamic-menu.service';
 
+import { Column } from '@shared/interfaces/column.interface';
+
 import { ReportModeSwitcherComponent } from '@task/components/report-mode-switcher/report-mode-switcher.component';
+import { columns as totalModelColumns } from '@task/constants/report-total-columns.constant';
 import { ReportModeEnum } from '@task/enums/report-mode.enum';
 import { Task } from '@task/models/task.model';
 import { ReportService } from '@task/services/report.service';
@@ -21,24 +21,12 @@ import { TasksService } from '@task/services/tasks.service';
   templateUrl: './report.component.html',
   styleUrls: ['./report.component.scss'],
 })
-export class ReportComponent implements OnInit, AfterViewInit, OnDestroy {
-  public dataSource: MatTableDataSource<Task> = new MatTableDataSource<Task>([]);
+export class ReportComponent implements OnInit {
+  public tasks$: Observable<Task[]>;
+  public columns: Column[] = [];
 
-  public columns: string[] = [];
-
-  @ViewChild(MatSort)
-  private sort!: MatSort;
-  @ViewChild(MatPaginator)
-  private paginator!: MatPaginator;
-
-  private tasksSubscription!: Subscription;
-
-  private modeColumns: { [key: string]: string[] } = {
-    total: [
-      'name',
-      'description',
-      'timeLoggedToday',
-    ],
+  private modeColumns: { [key: string]: Column[] } = {
+    total: totalModelColumns,
   };
 
   constructor(
@@ -62,11 +50,7 @@ export class ReportComponent implements OnInit, AfterViewInit, OnDestroy {
       ),
     );
 
-    this.tasksSubscription = this.tasksService.tasks$
-                                 .pipe(
-                                   tap((data: Task[]) => this.dataSource.data = data),
-                                 )
-                                 .subscribe();
+    this.tasks$ = this.tasksService.tasks$;
 
     this.reportService.reportMode$
         .subscribe(
@@ -80,14 +64,5 @@ export class ReportComponent implements OnInit, AfterViewInit, OnDestroy {
           take(1),
         )
         .subscribe();
-  }
-
-  public ngAfterViewInit(): void {
-    this.dataSource.sort = this.sort;
-    this.dataSource.paginator = this.paginator;
-  }
-
-  public ngOnDestroy(): void {
-    this.tasksSubscription.unsubscribe();
   }
 }
