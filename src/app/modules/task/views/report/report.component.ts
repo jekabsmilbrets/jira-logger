767f92012fc343be8135bb62c7dc3ea1129555b6
Jēkabs Milbrets <jekabs.milbrets@me.com>
2022-03-04 18:48:06 +0200

    JL-13: Updated View ReportComponent

--- src/app/modules/task/views/report/report.component.ts
+++ src/app/modules/task/views/report/report.component.ts
@@ -1,15 +1,18 @@
+import { formatDate } from '@angular/common';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 
-import { take, Observable, Subscription } from 'rxjs';
+import { take, Observable, Subscription, tap } from 'rxjs';
 
 import { DynamicMenu } from '@core/models/dynamic-menu';
 import { DynamicMenuService } from '@core/services/dynamic-menu.service';
 
 import { Column } from '@shared/interfaces/column.interface';
+import { ReadableTimePipe } from '@shared/pipes/readable-time.pipe';
 
 import { SharedModule } from '@shared/shared.module';
 
 import { ReportModeSwitcherComponent } from '@task/components/report-mode-switcher/report-mode-switcher.component';
+import { columns as monthModelColumns } from '@task/constants/report-month-columns.constant';
 import { columns as totalModelColumns } from '@task/constants/report-total-columns.constant';
 import { ReportModeEnum } from '@task/enums/report-mode.enum';
 import { Task } from '@task/models/task.model';
@@ -22,11 +25,12 @@ import { TasksService } from '@task/services/tasks.service';
   styleUrls: ['./report.component.scss'],
 })
 export class ReportComponent implements OnInit, OnDestroy {
-  public tasks$: Observable<Task[]>;
+  public tasks$!: Observable<Task[]>;
   public columns: Column[] = [];
 
-  private modeColumns: { [key: string]: Column[] } = {
+  private tableColumns: { [key: string]: Column[] } = {
     total: totalModelColumns,
+    month: [],
   };
 
   private reportModeSubscription!: Subscription;
@@ -36,12 +40,24 @@ export class ReportComponent implements OnInit, OnDestroy {
     private dynamicMenuService: DynamicMenuService,
     private reportService: ReportService,
   ) {
-    this.createDynamicMenu();
-
-    this.tasks$ = this.tasksService.tasks$;
   }
 
   public ngOnInit(): void {
+    this.createDynamicMenu();
+
+    const date = new Date();
+    const dateA = new Date('2022-03-01');
+    const dateB = new Date('2022-03-31');
+    this.tasks$ = this.tasksService.tasks$
+                      .pipe(
+                        tap((tasks: Task[]) => tasks.map((task: Task) => console.log({
+                          date,
+                          task,
+                          calcTimeLoggedForDate: task.calcTimeLoggedForDate(date),
+                          calcTimeLoggedForDate2: task.calcTimeLoggedForDate(dateB),
+                          calcTimeLoggedForDateRange: task.calcTimeLoggedForDateRange(dateA, dateB),
+                        }))),
+                      );
     this.reportModeSubscription = this.reportService.reportMode$
                                       .subscribe(
                                         (reportMode: ReportModeEnum) => this.switchReportMode(reportMode),
@@ -77,6 +93,62 @@ export class ReportComponent implements OnInit, OnDestroy {
   }
 
   private switchReportMode(reportMode: ReportModeEnum): void {
-    this.columns = this.modeColumns[reportMode];
+    switch (true) {
+      case reportMode === ReportModeEnum.total:
+        break;
+      case reportMode === ReportModeEnum.month:
+        this.tableColumns[reportMode] = this.generateMonthColumns();
+        break;
+    }
+
+    this.columns = this.tableColumns[reportMode];
+    console.log({columns: this.columns});
+  }
+
+  private generateMonthColumns(): Column[] {
+    const modifiedMonthModelColumns = [...monthModelColumns];
+    const dateA = new Date('2022-03-01');
+    const dateB = new Date('2022-03-31');
+    const currentDate = new Date(dateA);
+
+    while (currentDate < dateB) {
+      const curDate = currentDate.getDate();
+      const currentDate2 = new Date(currentDate.getTime());
+      modifiedMonthModelColumns.push(
+        {
+          columnDef: 'date-' + curDate,
+          header: formatDate(currentDate2, 'd. MMM', 'lv-LV'),
+          sortable: true,
+          visible: !([
+            0,
+            6,
+          ].includes(currentDate2.getDay())),
+          cell: (task: Task) => (new ReadableTimePipe())
+            .transform(
+              task.calcTimeLoggedForDate(currentDate2),
+              true,
+            ),
+        },
+      );
+
+      currentDate.setDate(curDate + 1);
+    }
+
+    modifiedMonthModelColumns.push(
+      {
+        columnDef: 'timeLogged',
+        header: 'Total Time Logged ' + formatDate(dateA, 'yyyy MMMM', 'lv-LV'),
+        sortable: true,
+        visible: true,
+        stickyEnd: true,
+        cell: (task: Task) => (new ReadableTimePipe())
+          .transform(
+            task.calcTimeLoggedForDateRange(dateA, dateB),
+            true,
+          ),
+      },
+    );
+
+    return modifiedMonthModelColumns;
   }
 }
