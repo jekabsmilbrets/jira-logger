5c3081dea661d9e400b11f6ad7959da5662beb1f
Jēkabs Milbrets <jekabs.milbrets@me.com>
2022-03-04 18:47:35 +0200

    JL-13: Created report-month-columns.constant.ts

---
+++ src/app/modules/task/constants/report-month-columns.constant.ts
@@ -0,0 +1,22 @@
+import { Column } from '@shared/interfaces/column.interface';
+import { columnValue } from '@shared/utils/column-value.util';
+
+import { Task } from '@task/models/task.model';
+
+export const columns: Column[] = [
+  {
+    columnDef: 'name',
+    header: 'Name',
+    sortable: true,
+    visible: true,
+    sticky: true,
+    cell: (task: Task) => columnValue(task, 'name'),
+  },
+  {
+    columnDef: 'description',
+    header: 'Description',
+    sortable: true,
+    visible: true,
+    cell: (task: Task) => columnValue(task, 'description'),
+  },
+];
