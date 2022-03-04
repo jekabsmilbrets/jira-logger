e368bfd35ac826100b47b4e88a26b1fc2ca6196a
Jēkabs Milbrets <jekabs.milbrets@me.com>
2022-03-04 23:52:05 +0200

    JL-13: Updated TaskComponent;

--- src/app/modules/task/components/task/task.component.ts
+++ src/app/modules/task/components/task/task.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output }      from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators, AbstractControl } from '@angular/forms';
 
-import { Observable, of, switchMap, take } from 'rxjs';
+import { Observable, take, switchMap, of } from 'rxjs';
 
 import { TaskTagsEnum } from '@task/enums/task-tags.enum';
 
@@ -28,30 +28,37 @@ export class TaskComponent implements OnInit {
 
   public editMode = false;
 
-  public formGroup: FormGroup = new FormGroup({
-    name: new FormControl(
-      undefined,
-      [Validators.required],
-      [
-        (control: AbstractControl) => this.tasks$
-                                          .pipe(
-                                            take(1),
-                                            switchMap(
-                                              (tasks: Task[]) => {
-                                                const value = control.value;
+  public formGroup: FormGroup = new FormGroup(
+    {
+      name: new FormControl(
+        undefined,
+        [Validators.required],
+        [
+          (control: AbstractControl) => this.tasks$
+                                            .pipe(
+                                              take(1),
+                                              switchMap(
+                                                (tasks: Task[]) => {
+                                                  const value = control.value;
 
-                                                if (tasks.find((task) => task.name === value && this.task.uuid !== task.uuid)) {
-                                                  return of({'duplicate-task': true});
-                                                }
+                                                  if (
+                                                    tasks.find(
+                                                      (task: Task) => task.name === value && this.task.uuid !== task.uuid,
+                                                    )
+                                                  ) {
+                                                    return of({'duplicate-task': true});
+                                                  }
 
-                                                return of(null);
-                                              },
+                                                  return of(null);
+                                                },
+                                              ),
                                             ),
-                                          ),
-      ]),
-    description: new FormControl(),
-    tags: new FormControl([TaskTagsEnum.opex]),
-  });
+        ],
+      ),
+      description: new FormControl(),
+      tags: new FormControl([TaskTagsEnum.opex]),
+    },
+  );
 
   public tags: { viewValue: string; value: TaskTagsEnum }[] = [
     {
@@ -75,15 +82,19 @@ export class TaskComponent implements OnInit {
   }
 
   public ngOnInit(): void {
-    this.formGroup.patchValue({
-      name: this.task.name,
-      description: this.task.description,
-      tags: this.task.tags,
-    });
+    this.formGroup.patchValue(
+      {
+        name: this.task.name,
+        description: this.task.description,
+        tags: this.task.tags,
+      },
+    );
   }
 
   public viewTag(tag: TaskTagsEnum): string {
-    return this.tags.find((sTag) => sTag.value === tag)?.viewValue ?? '';
+    return this.tags.find(
+      (sTag) => sTag.value === tag
+    )?.viewValue ?? '';
   }
 
   public onUpdate(task: Task): void {
@@ -91,12 +102,12 @@ export class TaskComponent implements OnInit {
     task.description = this.formGroup.get('description')?.value;
     task.tags = this.formGroup.get('tags')?.value ?? [];
 
-    console.log(task);
-
-    this.update.emit([
-      task,
-      TaskUpdateActionEnum.update,
-    ]);
+    this.update.emit(
+      [
+        task,
+        TaskUpdateActionEnum.update,
+      ],
+    );
   }
 
   public onRemove(task: Task): void {
@@ -113,17 +124,21 @@ export class TaskComponent implements OnInit {
       action = TaskUpdateActionEnum.stopWorkLog;
     }
 
-    this.update.emit([
-      task,
-      action,
-    ]);
+    this.update.emit(
+      [
+        task,
+        action,
+      ],
+    );
   }
 
   public toggleEditMode(): void {
     this.editMode = !this.editMode;
-    this.formGroup.patchValue({
-      name: this.task.name,
-      description: this.task.description,
-    });
+    this.formGroup.patchValue(
+      {
+        name: this.task.name,
+        description: this.task.description,
+      },
+    );
   }
 }
