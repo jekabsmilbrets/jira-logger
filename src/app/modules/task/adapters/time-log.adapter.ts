0c209230cad845a59807a38c4fe573c1377819ce
Jēkabs Milbrets <jekabs.milbrets@me.com>
2022-03-06 19:43:42 +0200

    JL-8: Created Task and TimeLog Adapters

---
+++ src/app/modules/task/adapters/time-log.adapter.ts
@@ -0,0 +1,15 @@
+import { TimeLogInterface } from '@task/interfaces/time-log.interface';
+import { TimeLog }          from '@task/models/time-log.model';
+
+export const adaptTimeLog = (dbTimeLog: TimeLogInterface): TimeLog => new TimeLog(
+  {
+    uuid: dbTimeLog._uuid,
+    endTime: dbTimeLog._endTime && new Date(dbTimeLog._endTime),
+    startTime: new Date(dbTimeLog._startTime),
+    description: dbTimeLog._description,
+  },
+);
+
+export const adaptTimeLogs = (dbTimeLogs: TimeLogInterface[]): TimeLog[] => dbTimeLogs.map(
+  (dbTimeLog: TimeLogInterface) => adaptTimeLog(dbTimeLog),
+);
