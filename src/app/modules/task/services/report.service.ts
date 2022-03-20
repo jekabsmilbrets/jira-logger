76c3fbb13dc9c62b81960b731b3e155083783ead
Jēkabs Milbrets <jekabs.milbrets@me.com>
2022-03-20 18:04:30 +0200

    JL-53: Updated ReportService;

--- src/app/modules/task/services/report.service.ts
+++ src/app/modules/task/services/report.service.ts
@@ -3,18 +3,32 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 
 import { ReportModeEnum } from '@task/enums/report-mode.enum';
+import { TaskTagsEnum }   from '@task/enums/task-tags.enum';
 
 @Injectable()
 export class ReportService {
   public reportMode$: Observable<ReportModeEnum>;
+  public tags$: Observable<TaskTagsEnum[]>;
 
   private reportModeSubject: BehaviorSubject<ReportModeEnum> = new BehaviorSubject<ReportModeEnum>(ReportModeEnum.total);
+  private tagsSubject: BehaviorSubject<TaskTagsEnum[]> = new BehaviorSubject<TaskTagsEnum[]>(
+    [
+      TaskTagsEnum.capex,
+      TaskTagsEnum.opex,
+      TaskTagsEnum.other,
+    ],
+  );
 
   constructor() {
     this.reportMode$ = this.reportModeSubject.asObservable();
+    this.tags$ = this.tagsSubject.asObservable();
   }
 
   public set reportMode(mode: ReportModeEnum) {
     this.reportModeSubject.next(mode);
+  }
+
+  public set tags(tags: TaskTagsEnum[]) {
+    this.tagsSubject.next(tags);
   }
 }
