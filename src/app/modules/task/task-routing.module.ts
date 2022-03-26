7b5051d25bbb350999c6077c4688c5a0c50feafb
Jēkabs Milbrets <jekabs.milbrets@me.com>
2022-03-26 23:15:35 +0200

    JL-83: Updated Task Module - removing Report remtrans;

--- src/app/modules/task/task-routing.module.ts
+++ src/app/modules/task/task-routing.module.ts
@@ -1,23 +1,13 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { ReportViewComponent } from './views/report/report-view.component';
-import { TasksViewComponent }  from './views/tasks/tasks-view.component';
+import { TasksViewComponent } from './views/tasks/tasks-view.component';
 
 const routes: Routes = [
   {
     path: '',
     redirectTo: 'list',
     pathMatch: 'full',
-  },
-  {
-    path: 'report',
-    redirectTo: 'report/total',
-    pathMatch: 'full',
-  },
-  {
-    path: 'report/:reportMode',
-    component: ReportViewComponent,
   },
   {
     path: 'list',
