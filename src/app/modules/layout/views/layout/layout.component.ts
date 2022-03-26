62db5a8ec59b2f8894dfebc27524e36ea46a7fb3
Jēkabs Milbrets <jekabs.milbrets@me.com>
2022-03-26 17:58:23 +0200

    JL-83: Updated Layout HeaderComponent, View LayoutComponent added loading progress bar;

--- src/app/modules/layout/views/layout/layout.component.ts
+++ src/app/modules/layout/views/layout/layout.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
 
+import { Observable } from 'rxjs';
+
+import { StorageService } from '@core/services/storage.service';
+
 @Component({
              selector: 'app-layout',
              templateUrl: './layout.component.html',
              styleUrls: ['./layout.component.scss'],
            })
 export class LayoutComponent {
+  public isLoading$: Observable<boolean>;
+
+  constructor(
+    private storageService: StorageService,
+  ) {
+    this.isLoading$ = this.storageService.isLoading$;
+  }
 }
