555a625a72934101659847c521fea64e6e094c40
Jēkabs Milbrets <jekabs.milbrets@me.com>
2022-09-23 22:52:08 +0300

    JL-Import-Re-Implement: Code fixes;

--- frontend/src/app/modules/task/data-validators/task-interface.validator.ts
+++ frontend/src/app/modules/task/data-validators/task-interface.validator.ts
@@ -1,9 +1,9 @@
 import { ApiTask } from '@shared/interfaces/api/api-task.interface';
 import { Tag }     from '@shared/models/tag.model';
 
-import { validateTimeLogsInterfaceData } from '@task/data-validators/time-log-interface.validator';
+import { validateTagsInterfaceData } from '@task/data-validators/tag-interface-data.validator';
 
-import { validateTagsInterfaceData } from '@task/data-validators/validate-tag-interface-data.validator';
+import { validateTimeLogsInterfaceData } from '@task/data-validators/time-log-interface.validator';
 
 
 export const validateTaskInterfaceData = (taskInterfaceData: any, tags: Tag[]): ApiTask => {
@@ -15,7 +15,7 @@ export const validateTaskInterfaceData = (taskInterfaceData: any, tags: Tag[]): 
     .forEach(
       (field: string) => {
         if (!taskInterfaceData.hasOwnProperty(field)) {
-          throw new Error(`Missing Required field "${field}" for Task "${taskInterfaceData?._name}"!`);
+          throw new Error(`Missing Required field "${field}" for Task!`);
         }
       },
     );
@@ -23,7 +23,7 @@ export const validateTaskInterfaceData = (taskInterfaceData: any, tags: Tag[]): 
   return {
     createdAt: taskInterfaceData._createDate,
     name: taskInterfaceData._name,
-    description: taskInterfaceData?._description,
+    description: taskInterfaceData._description,
     timeLogs: taskInterfaceData._timeLogs && validateTimeLogsInterfaceData(taskInterfaceData._timeLogs),
     tags: taskInterfaceData._tags && validateTagsInterfaceData(taskInterfaceData._tags, tags),
   } as ApiTask;
