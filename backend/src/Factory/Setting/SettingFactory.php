05409b34f4199ca66335578604147d15feac3341
Jēkabs Milbrets <jekabs.milbrets@me.com>
2022-10-29 22:51:44 +0300

    BE-Sync-to-JIRA: Created Setting - Controller, Service, Repository, Entity, DTO;

---
+++ backend/src/Factory/Setting/SettingFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Factory\Setting;
+
+use App\Dto\Setting\SettingRequest;
+use App\Entity\Setting\Setting;
+
+class SettingFactory
+{
+    final public static function create(
+        SettingRequest $settingRequest,
+        ?Setting       $setting = null,
+    ): Setting {
+        if (null === $setting) {
+            $setting = new Setting();
+        }
+
+        if (null !== ($name = $settingRequest->getName())) {
+            $setting->setName($name);
+        }
+
+        if (null !== ($value = $settingRequest->getValue())) {
+            $setting->setValue($value);
+        }
+
+        return $setting;
+    }
+}
