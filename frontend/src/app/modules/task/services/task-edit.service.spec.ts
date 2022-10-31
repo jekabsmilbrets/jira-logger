07309844517db9ecd34bdccdaa071a4ee5691a29
Jēkabs Milbrets <jekabs.milbrets@me.com>
2022-10-31 21:35:38 +0200

    FE-Sync-to-JIRA: Created TaskEditService that generates formGroup for editing Task;

---
+++ frontend/src/app/modules/task/services/task-edit.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { TaskEditService } from './task-edit.service';
+
+describe('TaskEditService', () => {
+  let service: TaskEditService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(TaskEditService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
