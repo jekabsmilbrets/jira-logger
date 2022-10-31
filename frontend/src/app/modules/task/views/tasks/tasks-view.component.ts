78b0f59dbf361f063465dedaf48e0e22e8e65fc0
Jēkabs Milbrets <jekabs.milbrets@me.com>
2022-10-31 21:39:28 +0200

    FE-Sync-to-JIRA: Updated TasksViewComponent code, styling and template;

--- frontend/src/app/modules/task/views/tasks/tasks-view.component.ts
+++ frontend/src/app/modules/task/views/tasks/tasks-view.component.ts
@@ -1,24 +1,22 @@
 import { Component, OnInit }                                   from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
 
-import { concat, Observable, of, switchMap, take } from 'rxjs';
+import { Observable, of, switchMap, take, forkJoin, NEVER } from 'rxjs';
 
 import { DynamicMenu }        from '@core/models/dynamic-menu';
 import { DynamicMenuService } from '@core/services/dynamic-menu.service';
 
-import { Tag } from '@shared/models/tag.model';
-
+import { Tag }             from '@shared/models/tag.model';
 import { Task }            from '@shared/models/task.model';
 import { TimeLog }         from '@shared/models/time-log.model';
 import { TagsService }     from '@shared/services/tags.service';
 import { TasksService }    from '@shared/services/tasks.service';
 import { TimeLogsService } from '@shared/services/time-logs.service';
+import { SharedModule }    from '@shared/shared.module';
 
-import { SharedModule } from '@shared/shared.module';
-
-import { TasksMenuComponent } from '@task/components/tasks-menu/tasks-menu.component';
-
+import { TasksMenuComponent }   from '@task/components/tasks-menu/tasks-menu.component';
 import { TaskUpdateActionEnum } from '@task/enums/task-update-action.enum';
+
 import { TasksSettingsService } from '@task/services/tasks-settings.service';
 
 
@@ -32,6 +30,7 @@ import { TasksSettingsService } from '@task/services/tasks-settings.service';
 export class TasksViewComponent implements OnInit {
   public tasks$: Observable<Task[]>;
   public isLoading$: Observable<boolean>;
+  public tags$: Observable<Tag[]>;
 
   public createTaskForm: FormGroup<{
     name: FormControl<string | null>;
@@ -71,8 +70,6 @@ export class TasksViewComponent implements OnInit {
     },
   );
 
-  public tags$: Observable<Tag[]>;
-
   constructor(
     private tasksService: TasksService,
     private timeLogsService: TimeLogsService,
@@ -99,7 +96,33 @@ export class TasksViewComponent implements OnInit {
     this.reloadData();
   }
 
-  public createTask(): void {
+  public onAction([task, action]: [Task, TaskUpdateActionEnum]): void {
+    let action$: Observable<TimeLog> = NEVER;
+    switch (action) {
+      case TaskUpdateActionEnum.startWorkLog:
+        action$ = this.startTimeLog(task);
+        break;
+
+      case TaskUpdateActionEnum.stopWorkLog:
+        if (task.isTimeLogRunning && task.lastTimeLog instanceof TimeLog) {
+          action$ = this.stopTimeLog(task, task.lastTimeLog);
+        }
+        break;
+
+      default:
+        break;
+    }
+
+    action$
+      .pipe(
+        take(1),
+        switchMap(() => this.tasksService.list()),
+        take(1),
+      )
+      .subscribe();
+  }
+
+  public onCreate(): void {
     const task = new Task(
       this.createTaskForm.getRawValue() as Partial<Task>,
     );
@@ -117,57 +140,16 @@ export class TasksViewComponent implements OnInit {
         );
   }
 
-  public onUpdate(
-    [
-      task,
-      action,
-    ]: [
-      Task,
-      TaskUpdateActionEnum
-    ],
-  ): void {
-    this.tasks$
-        .pipe(
-          take(1),
-          switchMap(
-            (tasks: Task[]) => {
-              const observables = [];
-
-              if (TaskUpdateActionEnum.startWorkLog === action) {
-                tasks.forEach(
-                  (t: Task) => {
-                    if (t.id !== task.id) {
-                      if (t.lastTimeLog && !t.lastTimeLog.endTime) {
-                        t.lastTimeLog.endTime = new Date();
-                        observables.push(this.timeLogsService.update(t, t.lastTimeLog));
-                      }
-                    }
-                  },
-                );
-              }
-
-              observables.push(this.tasksService.update(task));
-
-              return concat(...observables)
-                .pipe(
-                  take(observables.length),
-                );
-            },
-          ),
-        )
+  public onUpdate(task: Task): void {
+    this.tasksService.update(task)
+        .pipe(take(1))
         .subscribe();
   }
 
   public onRemove(task: Task): void {
     this.tasksService.delete(task)
-        .pipe(
-          take(1),
-        )
+        .pipe(take(1))
         .subscribe();
-  }
-
-  public onReload(): void {
-    this.reloadData();
   }
 
   public onCreateTimeLog([task, timeLog]: [Task, TimeLog]): void {
@@ -194,16 +176,58 @@ export class TasksViewComponent implements OnInit {
         .subscribe();
   }
 
-  private taskSort(a: Task, b: Task): number {
-    const mapDateTime = (timeLogs: TimeLog[]): number[] => timeLogs
-      .map(
-        (l: TimeLog) => l.startTime.getTime(),
-      );
+  private startTimeLog(
+    task: Task,
+  ): Observable<TimeLog> {
+    const timeLog: TimeLog = new TimeLog(
+      {
+        startTime: new Date(),
+      },
+    );
 
-    const aLastTimeLog = Math.max(...mapDateTime(a.timeLogs), -1);
-    const bLastTimeLog = Math.max(...mapDateTime(b.timeLogs), -1);
+    const createTimeLog$ = this.timeLogsService.create(task, timeLog);
 
-    return bLastTimeLog - aLastTimeLog;
+    return this.tasks$
+               .pipe(
+                 take(1),
+                 switchMap(
+                   (tasks: Task[]) => {
+                     const observables: Observable<TimeLog>[] = [];
+
+                     tasks.forEach(
+                       (oTask: Task) => {
+                         if (oTask.id !== task.id) {
+                           if (oTask.isTimeLogRunning && oTask.lastTimeLog instanceof TimeLog) {
+                             console.log({
+                                           oTask,
+                                           lastTimeLog: oTask.lastTimeLog,
+                                         });
+                             oTask.lastTimeLog.endTime = new Date();
+                             observables.push(this.timeLogsService.update(oTask, oTask.lastTimeLog));
+                           }
+                         }
+                       },
+                     );
+
+                     if (observables.length > 0) {
+                       return forkJoin(observables)
+                         .pipe(take(1));
+                     }
+
+                     return of(0);
+                   },
+                 ),
+                 switchMap(() => createTimeLog$),
+               );
+  }
+
+  private stopTimeLog(
+    task: Task,
+    timeLog: TimeLog,
+  ): Observable<TimeLog> {
+    timeLog.endTime = new Date();
+
+    return this.timeLogsService.update(task, timeLog);
   }
 
   private reloadData(): void {
@@ -234,5 +258,17 @@ export class TasksViewComponent implements OnInit {
         },
       ),
     );
+  }
+
+  private taskSort(a: Task, b: Task): number {
+    const mapDateTime = (timeLogs: TimeLog[]): number[] => timeLogs
+      .map(
+        (l: TimeLog) => l.startTime.getTime(),
+      );
+
+    const aLastTimeLog = Math.max(...mapDateTime(a.timeLogs), -1);
+    const bLastTimeLog = Math.max(...mapDateTime(b.timeLogs), -1);
+
+    return bLastTimeLog - aLastTimeLog;
   }
 }
