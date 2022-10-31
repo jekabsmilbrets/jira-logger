83cd401f5a5a2ee72a5979d08f1b1635cfa59ffc
Jēkabs Milbrets <jekabs.milbrets@me.com>
2022-10-31 21:39:01 +0200

    FE-Sync-to-JIRA: Updated TimeLogListModalComponent code and template;

--- frontend/src/app/modules/task/components/time-log-list-modal/time-log-list-modal.component.ts
+++ frontend/src/app/modules/task/components/time-log-list-modal/time-log-list-modal.component.ts
@@ -1,20 +1,19 @@
-import { Component, Inject, ViewChild }             from '@angular/core';
-import { FormControl }                              from '@angular/forms';
-import { MatDatepicker }                            from '@angular/material/datepicker';
-import { MAT_DIALOG_DATA, MatDialogRef, MatDialog } from '@angular/material/dialog';
+import { Component, ViewChild, Inject }  from '@angular/core';
+import { FormControl }                   from '@angular/forms';
+import { MatDatepicker }                 from '@angular/material/datepicker';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
-import { take, switchMap, of, throwError } from 'rxjs';
+import { take, throwError } from 'rxjs';
 
 import { Column }     from '@shared/interfaces/column.interface';
 import { Searchable } from '@shared/interfaces/searchable.interface';
 import { TimeLog }    from '@shared/models/time-log.model';
 
-import { TimeLogModalComponent } from '@task/components/time-log-list-modal/time-log-modal/time-log-modal.component';
-
 import { columns as timeLogListColumns }  from '@task/constants/time-log-list-columns.constant';
 import { TimeLogListDialogDataInterface } from '@task/interfaces/time-log-list-dialog-data.interface';
 import { TimeLogModalResponseInterface }  from '@task/interfaces/time-log-modal-response.interface';
 import { TimeLogsModalResponseInterface } from '@task/interfaces/time-logs-modal-response.interface';
+import { TimeLogEditService }             from '@task/services/time-log-edit.service';
 
 
 @Component(
@@ -32,109 +31,108 @@ export class TimeLogListModalComponent {
   @ViewChild('picker')
   public picker!: MatDatepicker<Date>;
 
-  private timeLogDialogRef!: MatDialogRef<TimeLogModalComponent, TimeLogModalResponseInterface | undefined>;
+  private createdTimeLogs: TimeLog[] = [];
+  private updatedTimeLogs: TimeLog[] = [];
+  private deletedTimeLogs: TimeLog[] = [];
 
   constructor(
+    private timeLogEditService: TimeLogEditService,
     private dialogRef: MatDialogRef<TimeLogListModalComponent, TimeLogsModalResponseInterface | undefined>,
     @Inject(MAT_DIALOG_DATA) public data: TimeLogListDialogDataInterface,
-    private dialog: MatDialog,
   ) {
   }
 
+  public get timeLogs(): TimeLog[] {
+    return this.data.task.timeLogs;
+  }
+
   public onCancel(): void {
-    this.dialogRef.close(
-      {
-        responseType: 'cancel',
-      },
-    );
+    this.dialogRef.close();
   }
 
   public onSave(): void {
+    const filterFinalTimeLogs = (timeLog: TimeLog) => this.timeLogs.find((timeLog2: TimeLog) => timeLog === timeLog2) instanceof TimeLog;
+
     this.dialogRef.close(
       {
-        responseType: 'update',
-        responseData: this.data.task.timeLogs,
+        created: this.createdTimeLogs.filter(filterFinalTimeLogs),
+        updated: this.updatedTimeLogs.filter(filterFinalTimeLogs),
+        deleted: this.deletedTimeLogs,
       },
     );
   }
 
   public onCellClick([timeLog, column]: [Searchable, Column]): void {
-    this.timeLogDialogRef = this.dialog.open(
-      TimeLogModalComponent,
-      {
-        data: {
-          task: this.data.task,
-          timeLog,
-        },
-      },
-    );
-
-    let oldTimeLog: TimeLog | undefined;
-
-    this.timeLogDialogRef.afterClosed()
-        .pipe(
-          take(1),
-          switchMap(
-            (result: TimeLogModalResponseInterface | undefined) => {
-              if (!result) {
-                result = {
-                  responseType: 'cancel',
-                };
-              }
-
-              switch (result.responseType) {
+    this.timeLogEditService.openTimeLogDialog(
+          timeLog as TimeLog,
+        )
+        .pipe(take(1))
+        .subscribe(
+          (response: TimeLogModalResponseInterface | undefined) => {
+            if (response) {
+              switch (response.responseType) {
                 case 'cancel':
                   break;
-
-                case 'delete':
-                  if (!result.hasOwnProperty('responseData')) {
-                    return throwError(() => new Error('Missing response data'));
-                  }
-
-                  oldTimeLog = result.responseData?.oldTimeLog;
-
-                  if (oldTimeLog) {
-                    this.removeTimeLog(oldTimeLog);
-
-                    return of(true);
+                case 'update':
+                  if (response.responseData) {
+                    this.onUpdateAction(response.responseData);
                   }
                   break;
-
-                case 'update':
-                  if (
-                    !result.hasOwnProperty('responseData') ||
-                    (
-                      result.hasOwnProperty('responseData') && (
-                        !result.responseData?.hasOwnProperty('oldTimeLog') ||
-                        !result.responseData?.hasOwnProperty('updatedTimeLogData')
-                      )
-                    )
-                  ) {
-                    return throwError(() => new Error('Missing response data'));
-                  }
-
-                  oldTimeLog = result.responseData?.oldTimeLog;
-                  const updatedTimeLogData = result.responseData?.updatedTimeLogData ?? {};
-
-                  if (oldTimeLog) {
-
-                    this.updateTimeLog(oldTimeLog, updatedTimeLogData);
-
-                    return of(true);
-                  }
-
+                case 'delete':
+                  this.onRemoveAction(timeLog as TimeLog);
                   break;
               }
+            }
+          },
+        );
+  }
 
-              return of(false);
-            },
-          ),
-        )
-        .subscribe();
+  public onCreateAction(timeLog: TimeLog): void {
+    this.createdTimeLogs.push(timeLog);
+
+    const timeLogs = [...this.data.task.timeLogs];
+    timeLogs.push(timeLog);
+
+    this.data.task.timeLogs = timeLogs;
+  }
+
+  public onUpdateAction(timeLog: TimeLog): void {
+    this.updatedTimeLogs.push(timeLog);
+
+    const timeLogs = [...this.data.task.timeLogs];
+    const timeLogUuid = timeLog.id;
+    const indexOfTimeLog = this.data.task.timeLogs.findIndex(
+      (t: TimeLog) => t.id === timeLog.id,
+    );
+
+    if (indexOfTimeLog < 0) {
+      throwError(() => new Error(`Could not locate time log ${timeLogUuid}`));
+    }
+
+    timeLogs.splice(indexOfTimeLog, 1);
+    timeLogs.push(timeLog);
+
+    this.data.task.timeLogs = timeLogs;
   }
 
   public onRemoveAction(timeLog: Searchable): void {
-    this.removeTimeLog(timeLog as TimeLog);
+    if (timeLog.id) {
+      this.deletedTimeLogs.push(timeLog as TimeLog);
+    }
+
+    const timeLogs = [...this.data.task.timeLogs];
+    const timeLogUuid = timeLog.id;
+    const indexOfTimeLog = this.data.task.timeLogs.findIndex(
+      (t: TimeLog) => t.id === timeLog.id,
+    );
+
+    if (indexOfTimeLog < 0) {
+      throwError(() => new Error(`Could not locate time log ${timeLogUuid}`));
+    }
+
+    timeLogs.splice(indexOfTimeLog, 1);
+
+    this.data.task.timeLogs = timeLogs;
   }
 
   public onAddTimeLogClick(): void {
@@ -145,81 +143,24 @@ export class TimeLogListModalComponent {
       },
     );
 
-    this.timeLogDialogRef = this.dialog.open(
-      TimeLogModalComponent,
-      {
-        data: {
-          task: this.data.task,
+    this.timeLogEditService.openTimeLogDialog(
           timeLog,
-        },
-      },
-    );
-
-    this.timeLogDialogRef.afterClosed()
-        .pipe(
-          take(1),
-          switchMap(
-            (result: TimeLogModalResponseInterface | undefined) => {
-              if (!result) {
-                result = {
-                  responseType: 'cancel',
-                };
-              }
-
-              switch (result.responseType) {
+        )
+        .pipe(take(1))
+        .subscribe(
+          (response: TimeLogModalResponseInterface | undefined) => {
+            if (response) {
+              switch (response.responseType) {
                 case 'cancel':
                   break;
-
                 case 'update':
-                  if (!result.hasOwnProperty('responseData')) {
-                    return throwError(() => new Error('Missing response data'));
+                  if (response.responseData) {
+                    this.onCreateAction(response.responseData);
                   }
-
-                  this.createTimeLog(result.responseData?.updatedTimeLogData);
-
-                  return of(true);
+                  break;
               }
-
-              return of(false);
-            },
-          ),
-        )
-        .subscribe();
-  }
-
-  private removeTimeLog(timeLog: TimeLog): void {
-    const timeLogUuid = timeLog.id;
-    const indexOfTimeLog = this.data.task.timeLogs.findIndex(
-      (t: TimeLog) => t.id === timeLog.id,
-    );
-
-    if (indexOfTimeLog < 0) {
-      throwError(() => new Error(`Could not locate time log ${timeLogUuid}`));
-    }
-
-    const timeLogs = [...this.data.task.timeLogs];
-
-    timeLogs.splice(indexOfTimeLog, 1);
-
-    this.data.task.timeLogs = timeLogs;
-  }
-
-  private updateTimeLog(timeLog: TimeLog, newData: any): void {
-    const timeLogToUpdate: TimeLog | undefined = this.data.task.timeLogs.find(
-      (t: TimeLog) => t.id === timeLog.id,
-    );
-
-    if (timeLogToUpdate && newData) {
-      Object.assign(timeLogToUpdate, newData);
-    }
-  }
-
-  private createTimeLog(newTimeLogData: any): void {
-    const timeLog = new TimeLog(newTimeLogData);
-    const timeLogs = [...this.data.task.timeLogs];
-
-    timeLogs.push(timeLog);
-
-    this.data.task.timeLogs = timeLogs;
+            }
+          },
+        );
   }
 }
