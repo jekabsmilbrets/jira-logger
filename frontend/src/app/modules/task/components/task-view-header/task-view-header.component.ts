a1358b0e088fe43018fb825c16c5747e00623408
Jēkabs Milbrets <jekabs.milbrets@me.com>
2022-10-31 21:38:35 +0200

    FE-Sync-to-JIRA: Created TaskViewHeaderComponent with content projection;

---
+++ frontend/src/app/modules/task/components/task-view-header/task-view-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component(
+  {
+    selector: 'app-task-list-view-header',
+    templateUrl: './task-view-header.component.html',
+    styleUrls: ['./task-view-header.component.scss'],
+  },
+)
+export class TaskViewHeaderComponent {
+}
