d0a71a84397364142dd5489348a4e8223720a70b
Jēkabs Milbrets <jekabs.milbrets@me.com>
2022-11-12 16:34:37 +0200

    FE-Sync-to-JIRA: Created TaskCreateService, CreateTaskFromGroupInterface;

---
+++ frontend/src/app/modules/task/services/task-create.service.ts
@@ -0,0 +1,61 @@
+import { Injectable }                                                            from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, Validators, ValidationErrors } from '@angular/forms';
+
+import { of, switchMap, take, catchError, Observable, skip } from 'rxjs';
+
+import { Tag }          from '@shared/models/tag.model';
+import { Task }         from '@shared/models/task.model';
+import { TasksService } from '@shared/services/tasks.service';
+
+import { CreateTaskFromGroupInterface } from '@task/interfaces/create-task-from-group.interface';
+
+
+@Injectable()
+export class TaskCreateService {
+
+  constructor(
+    private tasksService: TasksService,
+  ) {
+  }
+
+  public createFormGroup(): FormGroup<CreateTaskFromGroupInterface> {
+    return new FormGroup<CreateTaskFromGroupInterface>(
+      {
+        name: new FormControl<string | null>(
+          null,
+          [Validators.required],
+          [
+            this.asyncValidator.bind(this),
+          ],
+        ),
+        description: new FormControl<string | null>(null),
+        tags: new FormControl<Tag[] | null>([]),
+      },
+    );
+  }
+
+  private asyncValidator(control: AbstractControl): Observable<ValidationErrors | null> {
+    const duplicateCheck = (value: string) => (tasks: Task[] | null) => {
+      if (
+        tasks &&
+        tasks.find(
+          (task: Task) => task.name === value,
+        )
+      ) {
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        return of({'duplicate-task': true});
+      }
+
+      return of(null);
+    };
+
+    return this.tasksService.tasks$
+               .pipe(
+                 skip(1),
+                 take(1),
+                 catchError(() => of(null)),
+                 switchMap(duplicateCheck(control.value)),
+               );
+  }
+
+}
