0ad52f7d343200eb68695cfd943eb0561b1061d3
Jēkabs Milbrets <jekabs.milbrets@me.com>
2022-11-14 21:39:07 +0200

    FE-Sync-to-JIRA: Moved Components to SharedModule: * ReportModeSwitcherComponent; * ReportTagFilterComponent; * ReportDateSelectorComponent; * ReportShowWeekendsComponent; * ReportHideUnreportedTasksComponent;

--- frontend/src/app/modules/shared/shared.module.ts
+++ frontend/src/app/modules/shared/shared.module.ts
@@ -1,10 +1,17 @@
-import { CommonModule } from '@angular/common';
-import { NgModule }     from '@angular/core';
+import { CommonModule }        from '@angular/common';
+import { NgModule }            from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AreYouSureDialogComponent } from '@shared/components/are-you-sure-dialog/are-you-sure-dialog.component';
 
-import { ErrorDialogComponent } from '@shared/components/error-dialog/error-dialog.component';
-import { TableComponent }       from '@shared/components/table/table.component';
+import { ErrorDialogComponent }               from '@shared/components/error-dialog/error-dialog.component';
+import { ReportDateSelectorComponent }        from '@shared/components/report-menu/report-date-selector/report-date-selector.component';
+// eslint-disable-next-line max-len
+import { ReportHideUnreportedTasksComponent } from '@shared/components/report-menu/report-hide-unreported-tasks/report-hide-unreported-tasks.component';
+import { ReportModeSwitcherComponent }        from '@shared/components/report-menu/report-mode-switcher/report-mode-switcher.component';
+import { ReportShowWeekendsComponent }        from '@shared/components/report-menu/report-show-weekends/report-show-weekends.component';
+import { ReportTagFilterComponent }           from '@shared/components/report-menu/report-tag-filter/report-tag-filter.component';
+import { TableComponent }                     from '@shared/components/table/table.component';
 
 import { ReadableTimePipe } from '@shared/pipes/readable-time.pipe';
 
@@ -21,6 +28,7 @@ import { MaterialModule } from '../material/material.module';
       CommonModule,
 
       MaterialModule,
+      ReactiveFormsModule,
     ],
     exports: [
       CommonModule,
@@ -29,6 +37,12 @@ import { MaterialModule } from '../material/material.module';
 
       TableComponent,
       ReadableTimePipe,
+
+      ReportTagFilterComponent,
+      ReportDateSelectorComponent,
+      ReportShowWeekendsComponent,
+      ReportHideUnreportedTasksComponent,
+      ReportModeSwitcherComponent,
     ],
     declarations: [
       TableComponent,
@@ -36,6 +50,12 @@ import { MaterialModule } from '../material/material.module';
       ReadableTimePipe,
       AreYouSureDialogComponent,
       ErrorDialogComponent,
+
+      ReportTagFilterComponent,
+      ReportDateSelectorComponent,
+      ReportShowWeekendsComponent,
+      ReportHideUnreportedTasksComponent,
+      ReportModeSwitcherComponent,
     ],
     providers: [
       AreYouSureService,
