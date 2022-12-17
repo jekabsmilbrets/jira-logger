bf02a30e82f5aab9c3b195fdb0506d2db49500fb
Jēkabs Milbrets <jekabs.milbrets@me.com>
2022-12-17 16:18:25 +0200

    QOL-FE: Updated LayoutComponent, show logged time for today;

--- frontend/src/app/modules/layout/views/layout/layout.component.ts
+++ frontend/src/app/modules/layout/views/layout/layout.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 
-import { Observable } from 'rxjs';
+import { BehaviorSubject, catchError, interval, map, Observable, of, switchMap, take, tap } from 'rxjs';
 
 import { LoaderStateService } from '@core/services/loader-state.service';
+
+import { Task }         from '@shared/models/task.model';
+import { TasksService } from '@shared/services/tasks.service';
 
 
 @Component(
@@ -14,10 +17,51 @@ import { LoaderStateService } from '@core/services/loader-state.service';
 )
 export class LayoutComponent {
   public isLoading$!: Observable<boolean>;
+  public timeLoggedToday$: Observable<number>;
+
+  private timeLoggedTodaySubject: BehaviorSubject<number> = new BehaviorSubject<number>(0);
 
   constructor(
     private loaderStateService: LoaderStateService,
+    private tasksService: TasksService,
   ) {
     this.isLoading$ = this.loaderStateService.isLoading$;
+    this.timeLoggedToday$ = this.timeLoggedTodaySubject.asObservable();
+    this.getTimeLoggedToday()
+      .pipe(
+        take(1),
+      )
+      .subscribe();
+
+    interval(10000)
+      .pipe(
+        switchMap(
+          () => this.getTimeLoggedToday()),
+      )
+      .subscribe();
+  }
+
+  public get date() {
+    const date = new Date();
+    date.setHours(0, 0, 0, 0);
+
+    return date;
+  };
+
+  private getTimeLoggedToday(): Observable<number> {
+    return this.tasksService.filteredList({
+      date: this.date,
+    })
+      .pipe(
+        catchError(() => of([])),
+        map((tasks: Task[]) => {
+          console.log([tasks]);
+          return tasks.map(
+            (task: Task) => task.calcTimeLoggedForDate(this.date),
+          )
+            .reduce((acc: number, value: number) => acc + value, 0);
+        }),
+        tap((timeLoggedToday: number) => this.timeLoggedTodaySubject.next(timeLoggedToday)),
+      );
   }
 }
