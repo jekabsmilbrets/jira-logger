1e791c3810204fcd8ed8398aa98ddfb124830621
Jēkabs Milbrets <jekabs.milbrets@me.com>
2026-05-31 00:56:10 +0300

    [FE-JL-Re-Fresh] backend: refresh dependencies and security/config baseline

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -12,4 +12,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Evotodi\SeedBundle\EvotodiSeedBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
