d3636ed9fb00f30fa004ab95ead38bd8bf06e15f
Jēkabs Milbrets <jekabs.milbrets@me.com>
2026-06-07 14:59:40 +0200

    [FE-JL-Re-Fresh] backend: modernize Symfony platform configuration

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -10,7 +10,6 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-    Evotodi\SeedBundle\EvotodiSeedBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
