722a6d15acb3345162cfa993ac4d14bfd7a17753
Jēkabs Milbrets <jekabs.milbrets@me.com>
2026-07-08 19:38:43 +0300

    [FE-improve-codebase-architecture] Collapse Report route duplication

--- frontend/src/app/report/report.routes.ts
+++ frontend/src/app/report/report.routes.ts
@@ -1,9 +1,22 @@
-import { Routes } from '@angular/router';
+import { type Route, Routes } from '@angular/router';
 
 import { HEADER_MENU_ROUTE_DATA_KEY } from '@layout/interfaces/header-menu-route-data.interface';
 
 import { ReportMenuComponent } from './components/report-menu/report-menu.component';
 import { reportResolver } from './resolvers/report.resolver';
+
+const reportViewRoute: (
+  path: string,
+) => Route = (
+  path: string,
+): Route => ({
+  path,
+  loadComponent: () => import('./views/report/report-view.component')
+    .then(m => m.ReportViewComponent),
+  resolve: {
+    settingsFromPath: reportResolver,
+  },
+});
 
 export const reportRoutes: Routes = [
   {
@@ -15,30 +28,9 @@ export const reportRoutes: Routes = [
       },
     },
     children: [
-      {
-        path: '',
-        loadComponent: () => import('./views/report/report-view.component')
-          .then(m => m.ReportViewComponent),
-        resolve: {
-          settingsFromPath: reportResolver,
-        },
-      },
-      {
-        path: ':reportMode',
-        loadComponent: () => import('./views/report/report-view.component')
-          .then(m => m.ReportViewComponent),
-        resolve: {
-          settingsFromPath: reportResolver,
-        },
-      },
-      {
-        path: ':reportMode/:date',
-        loadComponent: () => import('./views/report/report-view.component')
-          .then(m => m.ReportViewComponent),
-        resolve: {
-          settingsFromPath: reportResolver,
-        },
-      },
+      reportViewRoute(''),
+      reportViewRoute(':reportMode'),
+      reportViewRoute(':reportMode/:date'),
       {
         path: '**',
         redirectTo: '',
