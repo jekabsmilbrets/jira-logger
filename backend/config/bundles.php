1a2c5537693821ff2e7b1aa71a97df8127dd5787
Jēkabs Milbrets <jekabs.milbrets@me.com>
2026-07-25 02:36:20 +0300

    [improve-codebase-architecture] remove unused API authentication variables and security bundle

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -11,5 +11,4 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
-    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
